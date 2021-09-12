86269d1b855e3fe69f1f76e10f3c45c7ba280bc1
thais de rodez <thaisderodez@gmail.com>
2021-09-12 19:23:44 +0200

    added show for comic

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -2,4 +2,8 @@ class ComicsController < ApplicationController
   def index
     @comics = Comic.all
   end
+
+  def show
+    @comic = Comic.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_151208) do
+ActiveRecord::Schema.define(version: 2021_09_12_171301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,13 @@ ActiveRecord::Schema.define(version: 2021_09_11_151208) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "comic_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["comic_id"], name: "index_reviews_on_comic_id"
+  end
+
+  add_foreign_key "reviews", "comics"
 end
